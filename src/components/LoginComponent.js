fb63c29c29c78fd313f6db1ea95dedb75910e1e1
clarakim <kim.clar@husky.neu.edu>
2020-03-30 18:32:49 -0400

    stylized login page

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,52 +1,61 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./LoginComponent.css"
 
 class LoginComponent extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div id="wbdv-login-page-background">
+                <div id="wbdv-login-form-box" className="container">
 
-                {/*Username*/}
-                <div className="form-group row">
-                    <label htmlFor="username" className="col-sm-2 col-form-label">
-                        Username
-                    </label>
-                    <div className="col-sm-10">
-                        <input className="form-control"
-                               type="text"
-                               id="username"
-                               title="Username"
-                               placeholder="Your Username"/>
+                    <h2>Salutations, Word Nerd!</h2>
+                    <br/>
+
+                    {/*Username*/}
+                    <div className="form-group row">
+                        <label htmlFor="username" className="col-sm-2 col-form-label">
+                            Username
+                        </label>
+                        <div className="col-sm-10">
+                            <input className="form-control"
+                                   type="text"
+                                   id="username"
+                                   title="Username"
+                                   placeholder="Your Username"/>
+                        </div>
                     </div>
-                </div>
 
-                {/*Password*/}
-                <div className="form-group row">
-                    <label htmlFor="password" className="col-sm-2 col-form-label">
-                        Password
-                    </label>
-                    <div className="col-sm-10">
-                        <input className="form-control"
-                               type="password"
-                               id="password"
-                               title="Password"
-                               placeholder="********"/>
+                    {/*Password*/}
+                    <div className="form-group row">
+                        <label htmlFor="password" className="col-sm-2 col-form-label">
+                            Password
+                        </label>
+                        <div className="col-sm-10">
+                            <input className="form-control"
+                                   type="password"
+                                   id="password"
+                                   title="Password"
+                                   placeholder="********"/>
+                        </div>
                     </div>
-                </div>
 
-                {/*Sign In Button, Forgot Password, Sign Up*/}
-                <div className="form-group row">
-                    <label className="col-sm-2 col-form-label"></label>
-                    <div className="col-sm-10">
-                        <button className="btn btn-primary btn-block"
-                                // onClick= MAKE LOGIN FXN
-                        >
-                            Sign in
-                        </button>
-                        <Link to="/register" className="float-right" href="#">
-                            Not account? Sign up! (MAKE THIS INTO A BUTTON!)
-                        </Link>
+                    {/*Sign In Button, Forgot Password, Sign Up*/}
+                    <div className="form-group row">
+                        <label className="col-sm-2 col-form-label"></label>
+                        <div className="col-sm-10">
+                            <button className="btn btn-info btn-block"
+                                    // onClick= MAKE LOGIN FXN
+                            >
+                                Sign in
+                            </button>
+                            <br/>
+                            <Link to="/register" className="float-right" href="#">
+                                <button className="btn btn-dark btn-block">
+                                    No account? Sign up here!
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
