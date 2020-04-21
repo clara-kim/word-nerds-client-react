872740f4d9b9e54be2a44c9c976748687f72d4f4
clarakim <kim.clar@husky.neu.edu>
2020-04-21 18:39:38 -0400

    updated noted

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,2 @@
-//export const WN_API_URL = "https://polar-ravine-37796.herokuapp.com"
+//export const WN_API_URL = "https://polar-ravine-37796.herokuapp.com" // TODO DELETE
 export const WN_API_URL = "https://afternoon-citadel-15945.herokuapp.com"
