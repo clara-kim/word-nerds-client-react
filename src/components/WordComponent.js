726ecdbaab37ed09c2e349dcfa04e591712e370e
clarakim <kim.clar@husky.neu.edu>
2020-04-21 22:34:47 -0400

    removed all thumbs

--- src/components/WordComponent.js
+++ src/components/WordComponent.js
@@ -6,7 +6,6 @@ import quoteReducer from "../reducers/quoteReducer";
 import {combineReducers, createStore} from 'redux';
 import ContentSectionComponent from "./WordDetails/ContentSectionComponent";
 import DefinitionSectionComponent from "./WordDetails/DefinitionSectionComponent";
-import {getDislikeNumberForWord, getLikeNumberForWord} from "../services/wordService"
 import definitionReducer from "../reducers/definitionReducer";
 import sentenceReducer from "../reducers/sentenceReducer";
 import commentReducer from "../reducers/commentReducer";
@@ -28,47 +27,18 @@ class WordComponent extends React.Component {
     state = {
         wordData: [],
         definition:[],
-        likes: 0,
-        dislikes: 0,
-    }
-
-    // IF NOT SIGNED IN, CLICKING LIKE OR DISLIKE OPENS POP-UP
-    increaseLike = () => {
-        if (this.props.profile.userType === "PUBLIC") {
-            alert("You must be logged in to perform this action!")
-        }
-        else {
-            this.setState({likes: this.state.likes + 1})
-        }
-    }
-
-    increaseDislike = () => {
-        if (this.props.profile.userType === "PUBLIC") {
-            alert("You must be logged in to perform this action!")
-        }
-        else {
-            this.setState({dislikes: this.state.dislikes + 1})
-        }
     }
 
     componentDidMount = async() => {
         const wordData = await getWordDetails(this.props.word);
         this.setState({wordData: wordData});
         this.setState({definition: wordData.shortdef});
-        const likes = await getLikeNumberForWord(this.props.word);
-        const dislikes = await getDislikeNumberForWord(this.props.word);
-        this.setState({likes: likes});
-        this.setState({dislikes: dislikes});
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(prevProps.word !== this.props.word) {
             getWordDetails(this.props.word)
                 .then(result => this.setState({wordData: result[0]}));
-            getLikeNumberForWord(this.props.word)
-                .then(result => this.setState({likes: result}));
-            getDislikeNumberForWord(this.props.word)
-                .then(result => this.setState({dislikes: result}));
         }
     }
 
@@ -84,32 +54,6 @@ class WordComponent extends React.Component {
                             {/*WORD TITLE*/}
                             <span className="wbdv-word-title">
                                 {this.props.word}
-                            </span>
-
-                            {/*WORD THUMBS UP AND THUMBS DOWN*/}
-                            <span className="wbdv-word-thumbs">
-
-                                {/*THUMBS UP*/}
-                                <button className="wbdv-transparent-button"
-                                        onClick={ () => this.increaseLike()}>
-                                    <i className="fa fa-thumbs-up wbdv-word-like-icon"
-                                       title="like"/>
-                                    <br/>
-                                    <span className="wbdv-word-like-number">
-                                        {this.state.likes}
-                                    </span>
-                                </button>
-
-                                {/*THUMBS DOWN*/}
-                                <button className="wbdv-transparent-button"
-                                        onClick={ () => this.increaseDislike()}>
-                                    <i className="fa fa-thumbs-down wbdv-word-dislike-icon"
-                                       title="dislike"/>
-                                    <br/>
-                                    <span className="wbdv-word-dislike-number">
-                                        {this.state.dislikes}
-                                    </span>
-                                </button>
                             </span>
                         </h1>
 
--- src/components/WordDetails/SentenceComponent.js
+++ src/components/WordDetails/SentenceComponent.js
@@ -5,59 +5,15 @@ class SentenceComponent extends React.Component {
 
     state = {
         wordData: [],
-        likes: 9,
-        dislikes: 1,
         editing: false,
         content: this.props.content,
     }
 
-    // IF NOT SIGNED IN, CLICKING LIKE OR DISLIKE OPENS POP-UP
-    increaseLike = () => {
-        if (this.props.profile.userType === "PUBLIC") {
-            alert("You must be logged in to perform this action!")
-        }
-        else {
-            this.setState({likes: this.state.likes + 1})
-        }
-    }
-
-    increaseDislike = () => {
-        if (this.props.profile.userType === "PUBLIC") {
-            alert("You must be logged in to perform this action!")
-        }
-        else {
-            this.setState({dislikes: this.state.dislikes + 1})
-        }
-    }
-
-
     render(){
         return(
             <div className="container wbdv-tab-data">
-                {/*THUMBS UP AND THUMBS DOWN*/}
+
                 <p className="wbdv-tab-thumbs">
-
-                    {/*THUMBS UP*/}
-                    <button className="wbdv-transparent-button"
-                            onClick={()=>this.increaseLike()}>
-                        <i className="fa fa-thumbs-up wbdv-word-like-icon"
-                           title="like"/>
-                        <br/>
-                        <span className="wbdv-word-like-number">
-                            {this.state.likes}
-                        </span>
-                    </button>
-
-                    {/*THUMBS DOWN*/}
-                    <button className="wbdv-transparent-button"
-                            onClick={()=>this.increaseDislike()}>
-                        <i className="fa fa-thumbs-down wbdv-word-dislike-icon"
-                           title="dislike"/>
-                        <br/>
-                        <span className="wbdv-word-dislike-number">
-                            {this.state.dislikes}
-                        </span>
-                    </button>
 
                     {/*DELETE -- ONLY VISIBLE TO ADMIN AND UPLOADING USER*/}
                     {(this.props.profile.userType === "ADMIN" ||
